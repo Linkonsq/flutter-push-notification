c756783e224b0f2869e1d32db6600173ffb6c7af
Linkon Siddique <linkon@Linkons-MacBook-Pro.local>
2022-02-09 20:32:24 +0600

    Created push notifications project

---
+++ android/app/src/main/kotlin/com/linkonsiddique/push_notifications/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.linkonsiddique.push_notifications
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:push_notifications/green_page.dart';
+import 'package:push_notifications/red_page.dart';
+
+void main() {
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Push Notifications'),
+      routes: {
+        "red": (_) => const RedPage(),
+        "green": (_) => const GreenPage(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(12.0),
+        child: Center(
+          child: Text(
+            'You will receive push notifications',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
