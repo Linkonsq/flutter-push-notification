1af0198d4490b7a69bdaf0b7034d75c77cb3ec0c
Linkon <linkonsq>
2022-02-21 15:04:06 +0600

    Created local notifications

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
+import 'package:push_notifications/notifications.dart';
 import 'package:push_notifications/plant_stats_page.dart';
+import 'package:push_notifications/utilities.dart';
 import 'package:push_notifications/widgets.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +14,81 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Allow Notifications'),
+            content: const Text('Our app would like to send you notifications'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  'Don\'t Allow',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              TextButton(
+                  onPressed: () => AwesomeNotifications()
+                      .requestPermissionToSendNotifications()
+                      .then((_) => Navigator.pop(context)),
+                  child: const Text(
+                    'Allow',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+              )
+            ],
+          ),
+        );
+      }
+    });
+
+    AwesomeNotifications().createdStream.listen((notification) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(
+          'Notification Created on ${notification.channelKey}',
+        ),
+      ));
+    });
+
+    AwesomeNotifications().actionStream.listen((notification) {
+      if (notification.channelKey == 'basic_channel' && Platform.isIOS) {
+        AwesomeNotifications().getGlobalBadgeCounter().then(
+              (value) =>
+              AwesomeNotifications().setGlobalBadgeCounter(value - 1),
+        );
+      }
+
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+          builder: (_) => PlantStatsPage(),
+        ),
+            (route) => route.isFirst,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    AwesomeNotifications().actionSink.close();
+    AwesomeNotifications().createdSink.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +105,7 @@ class _HomePageState extends State<HomePage> {
                 ),
               );
             },
-            icon: Icon(
+            icon: const Icon(
               Icons.insert_chart_outlined_rounded,
               size: 30,
             ),
@@ -37,14 +116,21 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            PlantImage(),
-            SizedBox(
+            const PlantImage(),
+            const SizedBox(
               height: 25,
             ),
             HomePageButtons(
-              onPressedOne: () async {},
-              onPressedTwo: () async {},
-              onPressedThree: () async {},
+              onPressedOne: createPlantFoodNotification,
+              onPressedTwo: () async {
+                NotificationWeekAndTime? pickedSchedule =
+                await pickSchedule(context);
+
+                if (pickedSchedule != null) {
+                  createWaterReminderNotification(pickedSchedule);
+                }
+              },
+              onPressedThree: cancelScheduledNotifications,
             ),
           ],
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,32 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:push_notifications/home_page.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  AwesomeNotifications().initialize(
+    'resource://drawable/res_notification_app_icon',
+    [
+      NotificationChannel(
+        channelKey: 'basic_channel',
+        channelName: 'Basic Notifications',
+        channelDescription: 'Basic Notifications Channel',
+        defaultColor: Colors.teal,
+        importance: NotificationImportance.High,
+        channelShowBadge: true,
+      ),
+      NotificationChannel(
+        channelKey: 'scheduled_channel',
+        channelName: 'Scheduled Notifications',
+        channelDescription: 'Scheduled Notifications Channel',
+        defaultColor: Colors.teal,
+        locked: true,
+        importance: NotificationImportance.High,
+        soundSource: 'resource://raw/res_custom_notification',
+      ),
+    ]
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
